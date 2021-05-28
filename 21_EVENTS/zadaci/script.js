a57be4a8716c8420e2a811a7e1e3c9a0ca2b6dd2
vladimirbumbic <vladimirbumbic@gmail.com>
2021-05-28 16:32:56 +0200

    .

--- 21_EVENTS/zadaci/script.js
+++ 21_EVENTS/zadaci/script.js
@@ -47,4 +47,82 @@ minus.addEventListener("click", ()=>{
 });
 
 
+//5. Napraviti input polje i dugme.U input polje treba da se unese ime neke osobe, a na ekranu u paragrafu da se ispiše Zdravo i ime osobe preuzeto iz input polja.
 
+//DOM
+let btnPosalji = document.getElementById("posalji");
+let inputIme = document.getElementById("ime");
+let pPrikaziIme = document.getElementById("prikaziIme");
+
+btnPosalji.addEventListener("click", ()=>{
+    let upisanoIme = inputIme.value;
+    //console.log(upisanoIme);
+    pPrikaziIme.innerHTML = `Zdravo ${upisanoIme}`;
+});
+
+//6. Dopuniti 5. zadatak dodavanjem eventa i proizvoljnog alert-a, kada se klikne dva puta na dugme.
+
+btnPosalji.addEventListener("dblclick", ()=>{
+    let upisanoIme = inputIme.value;
+    alert(`Hello ${upisanoIme}`);
+});
+
+// 5. zadatak drugi nacin, preko forme
+let inputPrezime = document.getElementById("prezime");
+let btnPosaljiPrezime = document.getElementById("posaljiPrezime");
+let pPrikaziPrezime = document.getElementById("prikaziPrezime");
+
+btnPosaljiPrezime.addEventListener("click", (e)=>{  //ili e bez zagrade
+    e.preventDefault(); // sprecavamo podrazumevano ponasanje a to je da se rilouduje stranica
+    console.log(e);
+    pPrikaziPrezime.innerHTML = ` Uneli ste prezime ${inputPrezime.value}`;
+    inputPrezime.value = ""; // skloni napisano iz inputa
+});
+
+//7.
+let inputKvadriraj = document.getElementById("kvadriraj");
+let btnKv = document.getElementById("kva");
+
+let inputPrepolovi = document.getElementById("prepolovi");
+let btnPr = document.getElementById("pre");
+
+let inputPovrsina = document.getElementById("povrsina");
+let btnPo = document.getElementById("pov");
+
+let pJedan = document.getElementById("jedan");
+let pDva = document.getElementById("dva");
+let pTri = document.getElementById("tri");
+
+btnKv.addEventListener("click", (e)=>{
+    let broj= inputKvadriraj.value;
+    pJedan.innerHTML = `${broj**2}`;
+    e.preventDefault();
+});
+
+btnPr.addEventListener("click", e=>{
+    let broj= inputPrepolovi.value;
+    pDva.innerHTML = `${broj/2}`;
+    e.preventDefault();
+});
+
+btnPo.addEventListener("click", e=>{
+    let broj = inputPovrsina.value;
+    pTri.innerHTML = broj**2*3.14;
+    e.preventDefault();
+});
+
+//PRIMER
+let forma = document.getElementById("forma");
+let inputJelo = document.getElementById("jelo");
+let inputPice = document.getElementById("pice");
+let pIspis = document.getElementById("ispis");
+
+forma.addEventListener("submit", (e)=>{ //na formu zakacimo, a ne button, i onda forma slusa, click je za button
+    e.preventDefault();
+    pIspis.innerHTML = 
+   `<ul>
+        <li>Omiljeno jelo: ${inputJelo.value}</li>
+        <li>Omiljeno pice: ${inputPice.value}</li>
+    </ul>`
+    forma.reset(); //resetuje sva polja u formi na koja se odnosi
+});
